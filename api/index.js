a51eeba37be8c8afaf6792fc69f98a03bff1b23e
Julia Zhang <zhiyuan_zhang@trip.com>
2025-07-15 17:08:07 +0800

    feat: modify response for testing

--- api/index.js
+++ api/index.js
@@ -1,8 +1,27 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => res.send("Hello world"));
+app.get("/", async (req, res) => {
+  const data = await getData();
+  if (data) {
+    res.status(200).json({
+      q: data.setup,
+      a: data.punchline,
+    });
+  } else {
+    res.send('no data');
+  }
+});
 
 app.listen(3000, () => console.log("Server ready on port 3000."));
 
 module.exports = app;
+
+async function getData() {
+  try {
+    const res = await fetch("https://official-joke-api.appspot.com/random_joke");
+    return await res.json();
+  } catch (error) {
+    return null;
+  }
+}
